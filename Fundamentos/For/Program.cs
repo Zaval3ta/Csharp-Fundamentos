e54a07bf6e1cbb643b28596d57fdcfda57192c69
Cesar Zavaleta Estrada <zavaletap2210@gmail.com>
2025-03-19 18:03:44 -0600

    For en C#

---
+++ Fundamentos/For/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Friends = new string[6]
+            {
+                "Juan",
+                "Pedro",
+                "Pablo",
+                "Maria",
+                "Jose",
+                "Jorge"
+            };
+            bool run = true;
+            for (int i = 0; i<Friends.Length && run; i++)
+            {
+                Console.WriteLine(Friends[i]);
+            }
+        }
+    }
+}

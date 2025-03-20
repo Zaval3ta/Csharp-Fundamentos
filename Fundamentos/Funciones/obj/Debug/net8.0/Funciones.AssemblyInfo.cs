a3d6606c590be20811ccc7c92e4b0921dbea50c7
Cesar Zavaleta Estrada <zavaletap2210@gmail.com>
2025-03-19 18:06:49 -0600

    archivos modificados

--- Fundamentos/Funciones/obj/Debug/net8.0/Funciones.AssemblyInfo.cs
+++ Fundamentos/Funciones/obj/Debug/net8.0/Funciones.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Funciones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+840a8ce5ebcc4d5c04adcf12fd4c28a69bbdd344")]
 [assembly: System.Reflection.AssemblyProductAttribute("Funciones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Funciones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

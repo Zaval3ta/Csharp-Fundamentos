dc10558834d490c4f9b8cae52d97c46765ccdb7c
Cesar Zavaleta Estrada <zavaletap2210@gmail.com>
2025-03-19 18:03:01 -0600

    Sentencua de if else en C#

---
+++ Fundamentos/SentenciaIfElse/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace SentenciaIfElse
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            bool areYouHungry = true;
+            bool youHaveMoney = true;
+
+            if(areYouHungry && youHaveMoney && IsOpenRestaurant("McDonalds", 9))
+            {
+                System.Console.WriteLine("Puedes comer");
+            }
+            else
+            {
+                System.Console.WriteLine("No puedes comer");
+            }
+        }
+
+        static bool IsOpenRestaurant(string name, int hour = 0)
+        {
+            if(name == "McDonalds" && hour > 8 && hour < 23)
+            {
+                return true;
+            }
+            else if(name == "Restaurante 24 horas")
+            {
+                return true;
+            }else
+            { return false; }
+        }
+    }
+}
